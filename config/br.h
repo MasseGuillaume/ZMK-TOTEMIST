368f6217f465032bd4ef7315efa1867078fadee4
Guillaume Massé <masse.guillaume@narrative.io>
2025-08-10 13:24:05 -0400

    use abnt2

---
+++ config/br.h
@@ -0,0 +1,18 @@
+#define BR_QUOT GRAVE         // ` -> '
+#define BR_DQUO LS(BR_QUOT)   // ~ -> "
+#define BR_DIAE LS(N6)        // ^ -> ¨ (dead)
+#define BR_ACUT LEFT_BRACKET  // [ -> ´ (dead)
+#define BR_GRV  LS(BR_ACUT)   // ` (dead)
+#define BR_LBKT RIGHT_BRACKET // ] -> [
+#define BR_LCBR LS(BR_LBKT)   // {
+#define BR_RBRC BACKSLASH     // | -> ]
+#define BR_RCBR LS(BR_RBRC)   // }
+#define BR_CCED SEMICOLON     // ; -> Ç
+#define BR_TILD SINGLE_QUOTE  // ' -> ~ (dead)
+#define BR_CIRC LS(BR_TILD)   // ^ (dead)
+#define BR_BSLS NUBS          // backslash
+#define BR_PIPE LS(BR_BSLS)   // |
+#define BR_SLSH KC_INT1       // forward slash
+#define BR_QUES LS(BR_SLSH)   // ?
+#define BR_SCLN SLASH         // / -> ;
+#define BR_COLN LS(BR_SCLN)   // ? -> :
